b5c5933a1913d71dd7e13c3ef129ce1f3bfdaccb
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2025-06-08 20:27:11 +0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
-app.options('*', cors(corsOptions)); 
+// app.options('*', cors(corsOptions)); 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -49,7 +49,7 @@ AppDataSource.initialize()
     // Error handler
     app.use(globalErrorHandling);
 
-    const server = app.listen(port, '0.0.0.0', () => {
+    const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
     server.on('error', (error) => {
