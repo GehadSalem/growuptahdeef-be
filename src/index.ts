4d6b79a7d9d3e20cc6ecbaedba8f1984962b3108
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2025-06-08 19:40:43 +0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ AppDataSource.initialize()
     // Error handler
     app.use(globalErrorHandling);
 
-    const server = app.listen(port, () => {
+    const server = app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
     server.on('error', (error) => {
