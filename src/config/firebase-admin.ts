16c6293b82cafdbe2626a452088dfd09f247ab86
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2025-06-27 15:45:14 +0300

    commit

--- src/config/firebase-admin.ts
+++ src/config/firebase-admin.ts
@@ -2,12 +2,13 @@ import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert({
-            projectId:"growup-tahdeef-436e0",
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        }),
-    });
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  }),
+    // You can add other config options here if needed
+  });
 }
 
 export default admin;
